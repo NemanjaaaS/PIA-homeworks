48606c62a9c4120f5b9c8edc0142f0e8f13f3bae
NemanjaaaS <66557612+NemanjaaaS@users.noreply.github.com>
2020-12-20 23:59:27 +0100

    Drugi commit
    Dodat je blok sa instrukcijama kviza i dodeljene funkcije za dugmice u oba bloka

--- hw-3/Domaci PIA 3/js/main.js
+++ hw-3/Domaci PIA 3/js/main.js
@@ -1,3 +1,18 @@
+const next_btn = document.querySelector(".home-box button");
+const info_box = document.querySelector(".instruction-box");
+const home_box = document.querySelector(".home-box");
+const start_btn = document.querySelector(".instruction-box button");
 
+
+
+// ako je kliknuto next dugme
+next_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");	//prikazi blok sa instrukcijama
+	home_box.classList.add("hideHome");     //sakrij home
+}
+
+start_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+}
 
 
